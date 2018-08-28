c1625ad346a2d16c4da842ea2e70da3d21279c91
Shampu <cbuchhauser@gmail.com>
2018-08-27 20:59:25 -0700

    Just edited example model file

--- models/example.js
+++ models/example.js
@@ -1,7 +1,34 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var User = sequelize.define("User", {
+    // Giving the Author model a name of type STRING
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    email: DataTypes.STRING
   });
-  return Example;
+
+  var Trip = sequelize.define("Trip", {
+    // Giving the Author model a name of type STRING
+    name: DataTypes.STRING,
+    startDate: DataTypes.DATEONLY,
+    endDate: DataTypes.DATEONLY,
+    city: DataTypes.STRING
+  });
+
+  var Budget = sequelize.define("Budget", {
+    // Giving the Author model a name of type STRING
+    name: DataTypes.STRING,
+    category: DataTypes.STRING,
+    cost: DataTypes.DECIMAL
+  });
+
+  var Expense = sequelize.define("Expense", {
+    // Giving the Author model a name of type STRING
+    name: DataTypes.STRING,
+    category: DataTypes.STRING,
+    cost: DataTypes.DECIMAL
+  });
+
+  Trip.belongsTo(User);
+  Budget.belongsTo(Trip);
+  Expense.belongsTo(Trip);
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,24 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get all trips
+  app.get("/api/trips", function(req, res) {
+    db.Trip.findAll({}).then(function(plannerdb) {
+      res.json(plannerdb);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Create a new trip
+  app.post("/api/trips", function(req, res) {
+    db.Trip.create(req.body).then(function(plannerdb) {
+      res.json(plannerdb);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  // Delete a trip by id
+  app.delete("/api/trips/:id", function(req, res) {
+    db.Trip.destroy({ where: { id: req.params.id } }).then(function(plannerdb) {
+      res.json(plannerdb);
     });
   });
 };
