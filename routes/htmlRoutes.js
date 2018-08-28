f31e8493b1d32788818181d37153c5321c231229
Yamini Yadav <ykmba9@gmail.com>
2018-08-27 19:36:43 -0700

    added login page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,7 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.render("index");
   });
 
   // Load example page and pass in an example by id
