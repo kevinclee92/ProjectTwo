239af56be98d8f8b8e15ccad02bf766dbe59f316
bjl72 <bjlaney@alum.rpi.edu>
2018-08-29 19:49:44 -0700

    message

--- models/index.js
+++ models/index.js
@@ -4,17 +4,22 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "development";
+var env = process.env.NODE_ENV = "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable],
+    {dialect: "mysql"});
 } else {
   var sequelize = new Sequelize(
     process.env.DB_DATABASE,
+    console.log(process.env.DB_DATABASE),
     process.env.DB_USER,
+    console.log(process.env.DB_USER),
     process.env.DB_PASSWORD,
+    console.log(process.env.DB_PASSWORD),
+
     config
   );
 }
@@ -22,7 +27,7 @@ if (config.use_env_variable) {
 fs.readdirSync(__dirname)
   .filter(function(file) {
     return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === "index.js"
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
     );
   })
   .forEach(function(file) {
