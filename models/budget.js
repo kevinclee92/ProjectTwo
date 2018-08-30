62bb88c7e8cbd0e2adc7a68b5648afe38952eaf6
Kerry Tran <kurrytran32@gmail.com>
2018-08-29 19:24:46 -0700

    some

--- models/budget.js
+++ models/budget.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
   var Budget = sequelize.define("Budget", {
     // Giving the Author model a name of type STRING
-    tripId: DataTypes.INTEGER, 
+    tripId: DataTypes.INTEGER,
     dailyHotelCost: DataTypes.NUMERIC,
     dailyMealCost: DataTypes.NUMERIC,
     airfareCost: DataTypes.NUMERIC,
