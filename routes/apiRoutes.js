ec1426a2547e48e8ed0031df3adb0225a841580c
Kerry Tran <kurrytran32@gmail.com>
2018-09-03 14:09:07 -0700

    added city results page and able to grab data. formatting data to DOM objects later

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -29,6 +29,23 @@ module.exports = function(app) {
     });
   });
 
+  // getting the query where people search by city
+  app.get("/api/tripPlanning/:city", function(req, res) {
+    db.Trip.findAll({
+      where: { city: req.params.city }
+    }).then(function(cityOutput) {
+      res.json(cityOutput);
+    });
+  });
+  // getting the query where we grab the expense posts where user id is equal to tripId
+  app.get("/api/tripExpenses/:TripId", function(req, res) {
+    db.Expense.findAll({
+      where: { TripId: req.params.TripId }
+    }).then(function(cityOutput) {
+      res.json(cityOutput);
+    });
+  });
+
   // Delete an example by id
   app.delete("/api/tripPlanning/:id", function(req, res) {
     db.Trip.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
