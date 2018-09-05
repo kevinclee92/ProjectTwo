21390493e0510b41413403502c3eded45183784e
Kevin Lee <kevinclee92@gmail.com>
2018-09-04 18:55:51 -0700

    finished submissions page

--- public/js/entries.js
+++ public/js/entries.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
   var $entryContainer = $("#entriesArea");
 
+  getEntries();
 
 
   function getEntries() {
@@ -15,34 +16,34 @@ $(document).ready(function() {
   function createResults(dataOne, dataTwo) {
     $entryContainer.empty();
 
-    var $result = $(
-      [
-        '<div class="card">',
-        '<h5 class="card-header">Featured</h5>',
-        '<div class="card-body">',
-        '<h5 class="card-title">Special title treatment</h5>',
-        '<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>',
-        '<a href="#" class="btn btn-primary">Go somewhere</a>',
-        '</div>',
-        '</div>'
-      ].join("")
-    );
+    var results = [];
 
     for (var i = 0; i < dataOne.length; i++) {
       let tripId = dataOne[i].id;
 
-      
       for (var j = 0; j < dataTwo.length; j++) {
         if (dataTwo[j].TripId === tripId) {
 
-
+          var $result = $(
+            [
+              '<div class="card resultCard">',
+              '<h5 class="card-header center contentFont" style="font-weight: bold;">' + dataOne[i].city + '</h5>',
+              '<h5 class="card-header contentFont center">' + dataOne[i].startDate + '&nbsp; ~ &nbsp;' + dataOne[i].endDate + '</h5>',
+              '<div class="card-body">',
+              '<h5 class="card-title center contentFont"> Budget: <strong style="color: green;">$' + dataOne[i].initialBudget + '</strong></h5>',
+              '<ul class="contentFont">' + '<li>Lodging:  $' + dataTwo[j].dailyHotelExpense + '</li>' + '<li>Food:  $' + dataTwo[j].dailyMealExpense + '</li>' + '<li>Airfare:  $' + dataTwo[j].airfareExpense + '</li>' + '<li>Transportation:  $' + dataTwo[j].transportationExpense + '</li>' + '<li>Miscellaneous Costs:  $' + dataTwo[j].miscExpense + '</li>' + '</ul>',
+              '</div>',
+              '</div>'
+            ].join("")
+          );
+          
+          results.push($result);
 
         }
       }
-
-
     }
 
+    $entryContainer.append(results);
   }
 
 
