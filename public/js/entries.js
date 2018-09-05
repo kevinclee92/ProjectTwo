9440bbe5dce9c2264e47fcdb1935b8af071e4f36
Kerry Tran <kurrytran32@gmail.com>
2018-09-04 21:28:54 -0700

    fixed cityresults.js

--- public/js/entries.js
+++ public/js/entries.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
-
   var $entryContainer = $("#entriesArea");
 
   getEntries();
-
 
   function getEntries() {
     $.get("/api/tripPlanning", function(basicData) {
       $.get("/api/tripExpenses", function(detailData) {
         createResults(basicData, detailData);
-      })
-    })
-  };
+      });
+    });
+  }
 
   function createResults(dataOne, dataTwo) {
     $entryContainer.empty();
@@ -19,33 +17,52 @@ $(document).ready(function() {
     var results = [];
 
     for (var i = 0; i < dataTwo.length; i++) {
-      let id = dataTwo[i].TripId;
-      
+      var id = dataTwo[i].TripId;
 
       for (var j = 0; j < dataOne.length; j++) {
         if (dataOne[j].id === id) {
-
           var $result = $(
             [
               '<div class="card resultCard">',
-              '<h5 class="card-header center contentFont" style="font-weight: bold;">' + dataOne[j].city + '</h5>',
-              '<h5 class="card-header contentFont center">' + dataOne[j].startDate + '&nbsp; ~ &nbsp;' + dataOne[j].endDate + '</h5>',
+              '<h5 class="card-header center contentFont" style="font-weight: bold;">' +
+                dataOne[j].city +
+                "</h5>",
+              '<h5 class="card-header contentFont center">' +
+                dataOne[j].startDate +
+                "&nbsp; ~ &nbsp;" +
+                dataOne[j].endDate +
+                "</h5>",
               '<div class="card-body">',
-              '<h5 class="card-title center contentFont"> Budget: <strong style="color: green;">$' + dataOne[j].initialBudget + '</strong></h5>',
-              '<ul class="contentFont">' + '<li>Lodging:  $' + dataTwo[i].dailyHotelExpense + '</li>' + '<li>Food:  $' + dataTwo[i].dailyMealExpense + '</li>' + '<li>Airfare:  $' + dataTwo[i].airfareExpense + '</li>' + '<li>Transportation:  $' + dataTwo[i].transportationExpense + '</li>' + '<li>Miscellaneous Costs:  $' + dataTwo[i].miscExpense + '</li>' + '</ul>',
-              '</div>',
-              '</div>'
+              '<h5 class="card-title center contentFont"> Budget: <strong style="color: green;">$' +
+                dataOne[j].initialBudget +
+                "</strong></h5>",
+              '<ul class="contentFont">' +
+                "<li>Lodging:  $" +
+                dataTwo[i].dailyHotelExpense +
+                "</li>" +
+                "<li>Food:  $" +
+                dataTwo[i].dailyMealExpense +
+                "</li>" +
+                "<li>Airfare:  $" +
+                dataTwo[i].airfareExpense +
+                "</li>" +
+                "<li>Transportation:  $" +
+                dataTwo[i].transportationExpense +
+                "</li>" +
+                "<li>Miscellaneous Costs:  $" +
+                dataTwo[i].miscExpense +
+                "</li>" +
+                "</ul>",
+              "</div>",
+              "</div>"
             ].join("")
           );
-          
-          results.push($result);
 
+          results.push($result);
         }
       }
     }
 
     $entryContainer.append(results);
   }
-
-
 });
