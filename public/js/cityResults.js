737efbeeedc4a6b71aa24ad46b4bbff360b9d41e
Kerry Tran <kurrytran32@gmail.com>
2018-09-04 18:02:25 -0700

    city page working

--- public/js/cityResults.js
+++ public/js/cityResults.js
@@ -36,10 +36,61 @@ var userResults = function() {
     // nest everything in a for loop 
     for(let i = 0; i < userData.length; i++) {
       var userId = userData[i].id;
+      var userItem = userData[i];
       //grabbing the information for expense data
       cityAPI.grabExpenses(userId).then(function(expenseData) {
         // grabbing all the expense data for the filtered users and spitting them out to DOM
-        console.log(expenseData);
+        // test to grab individual values
+        console.log(expenseData[i].dailyHotelExpense);
+        //structuring card items
+        var $div = $("<div>");
+        // card class
+        var $card = $div.addClass("card");
+        var $cardHead = $("<div>").attr({
+          class: "card-header",
+          id: `heading${i}`});
+        var $h5 = $("<h5>").attr("class", "mb-0");
+        // adding bootstrap attr and user data into button
+        var $button = $("<button>").attr({
+          class: "btn btn-link accbtn",
+          "data-toggle" : "collapse",
+          "data-target" : `#collapse${i}`,
+          "aria-expanded" : "false",
+          "aria-controls" : `collapse${i}`
+        }).text(`${userItem.name}  ||  $${userItem.initialBudget}  ||  Duration ${userItem.startDate} - ${userItem.endDate}`);
+        // creating the div for the content of accordion
+        var $cardBodyDiv = $("<div>").attr({
+          id: `collapse${i}`,
+          class: "collapse",
+          "aria-labelledby": `heading${i}`,
+          "data-parent": "#accordion"
+        });
+        var $bodyDiv = $("<div>").addClass("card-body");
+        $bodyDiv.append(`
+          <ul class= "userExpense">
+            <li>Lodging: ${expenseData[i].dailyHotelExpense}</li>
+            <li>Food: ${expenseData[i].dailyMealExpense}</li>
+            <li>Airfare: ${expenseData[i].airfareExpense}</li>
+            <li>Transportation: ${expenseData[i].transportationExpense}</li>
+            <li>Miscellaneous Costs: ${expenseData[i].miscExpense}</li>
+        `)
+        // piecing together the appends
+        // appending button to h5 element
+        $h5.append($button);
+        // appending h5 to header
+        $cardHead.append($h5);
+        //appending header to card div
+        $card.append($cardHead);
+
+        // accordion body appends
+        //appending body div to cardbodydiv
+        $cardBodyDiv.append($bodyDiv);
+
+        // appending cardbody div below card
+        $card.append($cardBodyDiv);
+
+        //adding into the section
+        $("#accordion").append($card);
       });
     }
     
@@ -52,5 +103,6 @@ $("#searchSubmit").on("click", function() {
   $searchCity = $("#searchInput")
   .val()
   .trim();
+  $("#accordion").empty();
   userResults();
 });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -32,8 +32,8 @@ module.exports = function(app) {
 
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/tripPlanning/:id", function(req, res) {
+    db.Trip.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
       });
